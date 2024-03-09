36e7225cc237822be07f5632ebb414ad6e1b6125
NastyZl <zlatnasty20@gmail.com>
2024-03-09 15:33:00 +0300

    add UserEntity, AddressEntity, UserRepository

--- src/test/java/ru/nastyzl/fooddelivery/FoodDeliveryApplicationTests.java
+++ src/test/java/ru/nastyzl/fooddelivery/FoodDeliveryApplicationTests.java
@@ -1,15 +1,13 @@
 package ru.nastyzl.fooddelivery;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class FoodDeliveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
